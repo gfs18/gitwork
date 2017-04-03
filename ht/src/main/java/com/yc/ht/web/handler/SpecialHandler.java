3d3abaf1648e079afa44f2dbf1a14d335d08cbdf
Rozzzz <qq960089677@163.com>
2017-04-03 17:32:13 +0800

    点击专辑图片根据id显示详情、显示专辑中的歌曲  --罗昌玲

--- ht/src/main/java/com/yc/ht/web/handler/SpecialHandler.java
+++ ht/src/main/java/com/yc/ht/web/handler/SpecialHandler.java
@@ -1,17 +1,19 @@
 package com.yc.ht.web.handler;
 
-import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.yc.ht.entity.Song;
 import com.yc.ht.entity.Special;
 import com.yc.ht.service.SpecialService;
 
@@ -22,27 +24,38 @@ public class SpecialHandler {
 	@Autowired
 	public SpecialService specialService;
 	
-	@RequestMapping(value="",method=RequestMethod.GET)
+	@RequestMapping(value="index",method=RequestMethod.GET)
 	@ResponseBody
 	public List<Special> specialList(){
-		System.out.println("专辑图片显示进来了");
+		LogManager.getLogger().debug("专辑图片显示进来了");
 		return specialService.listSpeical();
 	}
 	
+	@RequestMapping(value="detail", method=RequestMethod.GET)
+	@ResponseBody
+	public Special specialDetail(int spid){
+		LogManager.getLogger().debug("根据专辑id获取详情。。。spid:"+spid);
+		return specialService.specialDetail(spid);
+	}
 	
+	@RequestMapping(value="music", method=RequestMethod.GET)
+	@ResponseBody
+	public List<Song> specialSong(int spid){
+		LogManager.getLogger().debug("根据专辑获取歌曲。。。");
+		return specialService.findSongBySpecial(spid);
+	}
 	
-	
-	
-	@RequestMapping(value="list",method=RequestMethod.POST)
+
+	/*@RequestMapping(value="list",method=RequestMethod.POST)
 	@ResponseBody
 	public List<Special> special_music(HttpServletRequest request){
 		Object obj = (Object)request.getParameter("param");
-		System.out.println("专辑详情进来了   obj:"+obj.toString());
+		LogManager.getLogger().debug("专辑详情进来了   obj:"+obj.toString());
 		try {
 			request.getRequestDispatcher("special_music.html").forward(request, null);
 		} catch (ServletException | IOException e) {
 			e.printStackTrace();
 		}
 		return null;
-	}
+	}*/
 }
