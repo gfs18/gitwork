1d873397b5fc3b4b72ee9400211dd1dbbf380894
Rozzzz <qq960089677@163.com>
2017-04-04 13:51:32 +0800

    专辑详情及包含的歌曲方法   --罗昌玲

--- ht/src/main/java/com/yc/ht/mapper/SpecialMapper.java
+++ ht/src/main/java/com/yc/ht/mapper/SpecialMapper.java
@@ -2,11 +2,16 @@ package com.yc.ht.mapper;
 
 import java.util.List;
 
+import com.yc.ht.entity.Song;
 import com.yc.ht.entity.Special;
 
 public interface SpecialMapper {
 
 	List<Special> findSpecial();
+
+	List<Song> findSongBySpecial(int spid);
+
+	Special specialDetail(int spid);
 
 	//List<Special> findSpecialMusic(); 
 	
