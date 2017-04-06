80e41e337836d4392a9e02571d03dceaf101d687
gfs18 <gfs13787475979@163.com>
2017-04-06 21:28:40 +0800

    分页

--- ht/src/main/webapp/js/manage.js
+++ ht/src/main/webapp/js/manage.js
@@ -1,15 +1,44 @@
+function showSongInfo(pageS,currP,count){
+	alert(pageS+"=========="+currP);
+	
+	$.get("song/pagination",{"pageS":pageS,"currP":currP},function(data){
+		var str = "";
+		for (var i = 0; i < data.rows.length; i++) {
+			str += "<tr class='tableoverout'><th>"+data.rows[i].soname+"</th><th>"+data.rows[i].sopicPath+"</th><th>"
+			+data.rows[i].sopubTime+"</th><th>"+data.rows[i].solyricPath+"</th><th>"
+			+data.rows[i].sopath+"</th><th>"+data.rows[i].soduration+"</th><th>"+data.rows[i].vipDownload+"</th></tr>";
+		}
+		str +='<script type="text/javascript">$(".tableoverout").mouseover(function(){this.style.backgroundColor="#30C27B";this.style.color="#ffffff";}); $(".tableoverout").mouseout(function(){this.style.backgroundColor="";this.style.color="#000000";});</script>';
+		$("#tableBody").html(str);
+		//alert(JSON.stringify(data));
+		paginationSong(data,count);
+	});
+}
+showSongInfo(5,1,0);
+
+function paginationSong(data,count){
+	var totalPage = data.totalPage;
+	var pStr = "";
+	if(totalPage>=5){
+		pStr +='<li><a href="javascript:void(0)" onclick="paginationSong('+data+','+( count<0?(count--):0 )+')" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
+		for (var j = 1; j <= 5; j++) {
+			pStr +='<li><a href="javascript:void(0)" onclick="showSongInfo(5,'+(j+(count*5))+','+count+')">'+(j+count*5)+'</a></li>';
+		}
+		pStr +='<li><a href="javascript:void(0)" onclick="paginationSong('+data+','+( count>=(data.totalPage/5)?(count++):data.totalPage )+')" aria-label="Next"> <span aria-hidden="true">&raquo;</span></a></li>';
+	}else{
+		pStr +='<li><a href="javascript:void(0)" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
+		for (var i = 1; i <= totalPage; i++) {
+			pStr +='<li class="active"><a href="javascript:void(0)" onclick="showSongInfo(5,'+i+')">'+i+'</a></li>';
+		}
+		pStr +='<li><a href="javascript:void(0)" aria-label="Next"> <span aria-hidden="true">&raquo;</span></a></li>';
+	}
+	alert(count);
+	$(".pagination").html(pStr);
+}
+
+
 $("#topnav li").mouseover(function(){
 	$(".mydiv").css("left",($(this).width())*$(this).index());
-});
-
-$("#tableBody tr").mouseover(function(){
-	this.style.backgroundColor="#30C27B";
-	this.style.color="#ffffff";
-});
-
-$("#tableBody tr").mouseout(function(){
-	this.style.backgroundColor="";
-	this.style.color="#000000";
 });
 
 function showDiv(){
@@ -30,14 +59,6 @@ function loginOut(){
 	},"json");
 }
 
-function showSongInfo(){
-	$.get("song/pagination",function(data){
-		alert(JSON.stringify(data));
-		
-		
-	});
-}
-showSongInfo();
 
 
 
