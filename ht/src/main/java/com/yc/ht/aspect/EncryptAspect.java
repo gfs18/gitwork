9fbaa07778d1f275a1ee05c46349300f91d42d64
CuteHuiHui <1363747715@qq.com>
2017-04-23 11:12:23 +0800

    前台用户的修改

--- ht/src/main/java/com/yc/ht/aspect/EncryptAspect.java
+++ ht/src/main/java/com/yc/ht/aspect/EncryptAspect.java
@@ -15,15 +15,27 @@ import com.yc.ht.util.Encrypt;
 @Aspect
 public class EncryptAspect {
 	
-	@Before("execution(* com.yc.ht.service.impl.UserServiceImpl.*(com.yc.ht.entity.Users))")
+/*	@Before("execution(* com.yc.ht.service.impl.UserServiceImpl.*(com.yc.ht.entity.Users))")
 	public void beforeMathod(JoinPoint joinPoint){
 		Users user= (Users) joinPoint.getArgs()[0];
 		LogManager.getLogger().debug("对密码加密中....");
 		user.setUpwd(Encrypt.md5AndSha(user.getUpwd()));
 		LogManager.getLogger().debug("加密成功....");
+	}*/
+	@Before("execution(* com.yc.ht.service.impl.UserServiceImpl.login(com.yc.ht.entity.Users))")
+	public void beforeMathod1(JoinPoint joinPoint){
+		Users user= (Users) joinPoint.getArgs()[0];
+		LogManager.getLogger().debug("对密码加密中....");
+		user.setUpwd(Encrypt.md5AndSha(user.getUpwd()));
+		LogManager.getLogger().debug("加密成功....");
 	}
-	
-	
+	@Before("execution(* com.yc.ht.service.impl.UserServiceImpl.zc(com.yc.ht.entity.Users))")
+	public void beforeMathod2(JoinPoint joinPoint){
+		Users user= (Users) joinPoint.getArgs()[0];
+		LogManager.getLogger().debug("对密码加密中....");
+		user.setUpwd(Encrypt.md5AndSha(user.getUpwd()));
+		LogManager.getLogger().debug("加密成功....");
+	}
 	@Before("execution(* com.yc.ht.service.impl.AdminServiceImpl.login(..))")
 	public void zc(JoinPoint joinPoint){
 		Admin admin= (Admin) joinPoint.getArgs()[0];
